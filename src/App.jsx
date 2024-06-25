41c2027bc728475705764d2fa2dae558557cdd50
Wiktor <lolxdyoloelo@gmail.com>
2024-06-25 21:13:34 +0200

    sdfsdf

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,52 @@
-import { useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
-import Canvas from "./components/canvas";
+
+function Canvas(props) {
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    // Clear the canvas
+    context.clearRect(0, 0, canvas.width, canvas.height);
+
+    // Draw the rectangle
+    context.fillStyle = "white";
+    context.fillRect(props.x, 10, 10, 10);
+  }, [props.x]); // Dependency array includes props.x
+
+  return <canvas ref={canvasRef} className="canvas"></canvas>;
+}
 
 function App() {
+  const [x, setX] = useState(10);
+
+  const handleClickLeft = () => {
+    setX((prevX) => prevX - 10);
+  };
+
+  const handleClickRight = () => {
+    setX((prevX) => prevX + 10);
+  };
   return (
     <>
       <div className="main">
         <div className="header">Witam wszystkich bardzo serdecznie</div>
         <div className="content">
-          <Canvas />
+          <Canvas x={x} />
         </div>
-        <div className="footer">Aplikacja do testuw</div>
+        <div className="footer">
+          <button className="moveButton" onClick={handleClickLeft}>
+            {"<"}
+          </button>
+          <button className="moveButton" onCanPlay={handleClickRight}>
+            {">"}
+          </button>
+        </div>
       </div>
     </>
   );
--- src/components/canvas.jsx
+++ src/components/canvas.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useRef } from "react";
+
 export default function Canvas() {
-  return (
-    <>
-      <canvas className="canvas"></canvas>
-    </>
-  );
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext("2d");
+
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    //prostokont
+    context.fillStyle = "white";
+    context.fillRect(10, 10, 10, 10);
+  }, []);
+
+  return <canvas ref={canvasRef} className="canvas"></canvas>;
 }
