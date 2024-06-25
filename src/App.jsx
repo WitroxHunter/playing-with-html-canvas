635e4a3d9e921377f48e5b1edd25edc8c7d8c230
Wiktor <lolxdyoloelo@gmail.com>
2024-06-25 21:16:29 +0200

    jupi

--- src/App.jsx
+++ src/App.jsx
@@ -16,14 +16,15 @@ function Canvas(props) {
 
     // Draw the rectangle
     context.fillStyle = "white";
-    context.fillRect(props.x, 10, 10, 10);
-  }, [props.x]); // Dependency array includes props.x
+    context.fillRect(props.x, props.y, 10, 10);
+  }, [props.x, props.y]); // Dependency array includes props.x and props.y
 
   return <canvas ref={canvasRef} className="canvas"></canvas>;
 }
 
 function App() {
   const [x, setX] = useState(10);
+  const [y, setY] = useState(10);
 
   const handleClickLeft = () => {
     setX((prevX) => prevX - 10);
@@ -32,16 +33,57 @@ function App() {
   const handleClickRight = () => {
     setX((prevX) => prevX + 10);
   };
+
+  const handleClickUp = () => {
+    setY((prevY) => prevY - 10);
+  };
+
+  const handleClickDown = () => {
+    setY((prevY) => prevY + 10);
+  };
+
+  const handleKeyDown = (event) => {
+    switch (event.key) {
+      case "ArrowLeft":
+        handleClickLeft();
+        break;
+      case "ArrowRight":
+        handleClickRight();
+        break;
+      case "ArrowUp":
+        handleClickUp();
+        break;
+      case "ArrowDown":
+        handleClickDown();
+        break;
+      default:
+        break;
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("keydown", handleKeyDown);
+    return () => {
+      window.removeEventListener("keydown", handleKeyDown);
+    };
+  }, []);
+
   return (
     <>
       <div className="main">
         <div className="header">Witam wszystkich bardzo serdecznie</div>
         <div className="content">
-          <Canvas x={x} />
+          <Canvas x={x} y={y} />
         </div>
         <div className="footer">
           <button className="moveButton" onClick={handleClickLeft}>
             {"<"}
+          </button>
+          <button className="moveButton" onClick={handleClickUp}>
+            {"^"}
+          </button>
+          <button className="moveButton" onClick={handleClickDown}>
+            {"v"}
           </button>
           <button className="moveButton" onClick={handleClickRight}>
             {">"}
@@ -51,5 +93,4 @@ function App() {
     </>
   );
 }
-
 export default App;
